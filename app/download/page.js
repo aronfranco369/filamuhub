d5a23ed9e8695b12e880173c67ab4f8227a5aa31
ARON FRANCO <aronfranco369@gmail.com>
2025-02-25 05:31:39 +0300

    hope it buids ; FUCK!!2

--- app/download/page.js
+++ app/download/page.js
@@ -1,9 +1,26 @@
+"use client";
+
+import { Suspense } from "react";
 import AppDownloadBanner from "../components/DownloadApp";
 
-export default function Page() {
+function DownloadContent() {
   return (
     <div className="min-h-screen bg-gray-900">
       <AppDownloadBanner />
     </div>
+  );
+}
+
+export default function Page() {
+  return (
+    <Suspense
+      fallback={
+        <div className="min-h-screen bg-gray-900 flex items-center justify-center text-white">
+          Loading...
+        </div>
+      }
+    >
+      <DownloadContent />
+    </Suspense>
   );
 }
