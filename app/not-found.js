deb745aeb2e9824731b8cfbc0fd6110b787a1fd1
ARON FRANCO <aronfranco369@gmail.com>
2025-02-25 05:42:09 +0300

    hope it buids ; FUCK!!6

---
+++ app/not-found.js
@@ -0,0 +1,20 @@
+// app/not-found.tsx
+import { Suspense } from "react";
+
+function NotFoundContent() {
+  // Any components using useSearchParams() should be inside this component
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+  );
+}
+
+export default function NotFound() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <NotFoundContent />
+    </Suspense>
+  );
+}
